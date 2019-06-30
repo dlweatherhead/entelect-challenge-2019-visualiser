5a65cb00e7578282f47cde10ee0a6772e6dfabad
David Weatherhead <dlweatherhead@gmail.com>
2019-06-30 20:04:50 +0200

    Started implementation on alternative match data representation that better handles getting data for each player.

--- Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/ReplayLoader.cs
+++ Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/ReplayLoader.cs
@@ -22,5 +22,49 @@ namespace EC2019 {
 
             roundsFinishedLoadingEvent?.Invoke(rounds);
         }
+
+        private void processDirectories(string absoluteDirectory) {
+            
+            List<Round> playerARounds = new List<Round>();
+            List<Round> playerBRounds = new List<Round>();
+            
+            // Loop through all rounds
+            var roundsList = Directory.GetDirectories(absoluteDirectory);
+
+            string playerAName;
+            string playerBName;
+
+            if (roundsList[0].Contains("A")) {
+                playerAName = roundsList[0];
+                playerBName = roundsList[1];
+            }
+            else {
+                playerAName = roundsList[1];
+                playerBName = roundsList[0];
+            }
+            
+            // Fetch each player
+            foreach (var round in roundsList) {
+                var playerFolders = Directory.GetDirectories(round);
+                foreach (var player in playerFolders) {
+                    var jsonMap = player + "/JsonMap.json";
+                    if (player.Contains("A")) {
+                        playerARounds.Add(new JsonFileParser<Round>(jsonMap).GetSerializedData());
+                    }
+
+                    if (player.Contains("B")) {
+                        playerBRounds.Add(new JsonFileParser<Round>(jsonMap).GetSerializedData());
+                    }
+                }
+            }
+            
+
+            // Add player round detail to player
+            Dictionary<string, List<Round>> playerRounds = new Dictionary<string, List<Round>>();
+            playerRounds.Add(playerAName, playerARounds);
+            playerRounds.Add(playerBName, playerBRounds);
+            
+            Debug.Log("Finished Loading all rounds for players");
+        }
     }
 }
