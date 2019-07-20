239b53a142e7c2ca439e9a00e755861eab1c17e0
David Weatherhead <dlweatherhead@gmail.com>
2019-07-20 23:02:33 +0200

    Added base tile update code. Implementation is a first ditch effort, destroying and creating new tiles on round update.

--- Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/ReplayManager.cs
+++ Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/ReplayManager.cs
@@ -10,9 +10,17 @@ namespace EC2019 {
 
         public static event NextRoundUpdateWorms nextRoundUpdateWormsEvent;
 
+        public delegate void NextRoundUpdateTiles();
+
+        public static event NextRoundUpdateTiles nextRoundUpdateTilesEvent;
+
         public ReplayRepo replayRepo;
         public float timePerRound = 1f;
         public float cameraMotionDelay = 0.5f;
+
+        public GameObject airTile;
+        public GameObject dirtTile;
+        public GameObject spaceTile;
 
         private List<Round> playerArounds;
         private List<Round> playerBrounds;
@@ -52,8 +60,9 @@ namespace EC2019 {
                 if (currentRound >= 2) {
                     if (singleCamera.isActiveAndEnabled) {
                         singleCamera.UpdateSize();
-                    } else if (dualCamera.isActiveAndEnabled) {
-                        dualCamera.UpdatePositions(playerAroundCurrentWormId, playerBroundCurrentWormId);                        
+                    }
+                    else if (dualCamera.isActiveAndEnabled) {
+                        dualCamera.UpdatePositions(playerAroundCurrentWormId, playerBroundCurrentWormId);
                     }
 
                     yield return new WaitForSeconds(cameraMotionDelay);
@@ -64,9 +73,40 @@ namespace EC2019 {
                     nextRoundUpdateWormsEvent(playerBround.Player);
                 }
 
+                DestroyTiles();
+                PopulateNextRoundTiles(playerAround.Map);
+
                 yield return new WaitForSeconds(timePerRound);
                 currentRound++;
             }
+        }
+
+        private void DestroyTiles() {
+            var tiles = GameObject.FindGameObjectsWithTag("Tile");
+            foreach (var tile in tiles) {
+                Destroy(tile);
+            }
+        }
+
+        private void PopulateNextRoundTiles(IEnumerable<List<Tile>> map) {
+            foreach (var row in map) {
+                foreach (var tile in row) {
+                    createTile(tile);
+                }
+            }
+        }
+
+        private void createTile(Tile tile) {
+            if (tile.TileType == TileType.AIR)
+                InstantiateObject(airTile, tile.X, tile.Y);
+            else if (tile.TileType == TileType.DIRT)
+                InstantiateObject(dirtTile, tile.X, tile.Y);
+            else if (tile.TileType == TileType.SPACE)
+                InstantiateObject(spaceTile, tile.X, tile.Y);
+        }
+
+        private static GameObject InstantiateObject(GameObject o, float x, float y) {
+            return Instantiate(o, new Vector3(x, 0f, y), Quaternion.identity);
         }
     }
 }
