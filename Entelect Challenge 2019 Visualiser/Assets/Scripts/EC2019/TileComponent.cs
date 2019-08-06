f6db55572cd0930c3b4fdcb57df2dfb219f55423
David Weatherhead <dlweatherhead@gmail.com>
2019-08-06 23:15:25 +0200

    Fixed Tile animation by preventing active tile from being set to false prior to re-activation if tile has not changed.

--- Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/TileComponent.cs
+++ Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/TileComponent.cs
@@ -1,34 +1,21 @@
 using EC2019.Entity;
-using EC2019.Utility;
 using UnityEngine;
 
 namespace EC2019 {
     public class TileComponent : MonoBehaviour {
-        public Tile tileDetail;
-
         public GameObject airTile;
         public GameObject dirtTile;
-        public GameObject spaceTile;
         public GameObject lavaTile;
+        public GameObject spaceTile;
 
         public void UpdateTile(Tile tile) {
-            airTile.SetActive(false);
-            dirtTile.SetActive(false);
-            spaceTile.SetActive(false);
-            lavaTile.SetActive(false);
-
             var y = gameObject.transform.position.y;
             transform.position = new Vector3(tile.X, y, tile.Y);
 
-            if (tile.TileType == TileType.AIR)
-                airTile.SetActive(true);
-            else if (tile.TileType == TileType.DIRT)
-                dirtTile.SetActive(true);
-            else if (tile.TileType == TileType.SPACE)
-                spaceTile.SetActive(true);
-            else if(tile.TileType == TileType.LAVA) {
-                lavaTile.SetActive(true);
-            }
+            airTile.SetActive(tile.TileType == TileType.AIR);
+            dirtTile.SetActive(tile.TileType == TileType.DIRT);
+            spaceTile.SetActive(tile.TileType == TileType.SPACE);
+            lavaTile.SetActive(tile.TileType == TileType.LAVA);
         }
     }
 }
