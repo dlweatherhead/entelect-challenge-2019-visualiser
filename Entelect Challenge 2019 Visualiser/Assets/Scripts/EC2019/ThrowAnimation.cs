26e704d805165b2f22f165a610931b169c9f767e
David Weatherhead <dlweatherhead@gmail.com>
2019-08-28 21:53:35 +0200

    Implemented animations for thrown items
    - Normal attack as grenade, snowball and banana bomb
    - Implemented mid round updates so that throw animations happen first
    and then the explosion animation and tiles update

---
+++ Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/ThrowAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace EC2019 {
+    public class ThrowAnimation : MonoBehaviour {
+        public Vector3 destination;
+
+        private float speed = 1f;
+
+        void Start() {
+            speed = Vector3.Distance(transform.position, destination);
+        }
+        
+        void Update() {
+            var step = 2 * speed * Time.deltaTime;
+                
+            transform.position = Vector3.MoveTowards(transform.position, destination, step);
+        
+            if (Vector3.Distance(transform.position, destination) < 0.001f) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
