3cb64607b7a1901ac8b8c429febc0b5629cfb5bc
David Weatherhead <dlweatherhead@gmail.com>
2019-09-04 20:21:34 +0200

    Added countdown sounds and text to loading screen

---
+++ Entelect Challenge 2019 Visualiser/Assets/Scripts/EC2019/IntroCountdown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class IntroCountdown : MonoBehaviour {
+
+    public TMP_Text countdown;
+    
+    void Start() {
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown() {
+        countdown.text = "5";
+        yield return new WaitForSeconds(1f);
+        countdown.fontSize += 15;
+        countdown.text = "4";
+        yield return new WaitForSeconds(1f);
+        countdown.fontSize += 15;
+        countdown.text = "3";
+        yield return new WaitForSeconds(1f);
+        countdown.fontSize += 15;
+        countdown.text = "2";
+        yield return new WaitForSeconds(1f);
+        countdown.fontSize += 15;
+        countdown.text = "1";
+        yield return new WaitForSeconds(1f);
+        countdown.fontSize += 15;
+        countdown.text = "!!!";
+    }
+}
